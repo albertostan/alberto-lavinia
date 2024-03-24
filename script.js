5eef3271fac95df1fd58254c48753bf8bd696df9
albertostan <164749452+albertostan@users.noreply.github.com>
2024-03-24 12:41:04 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var countDownDate = new Date("September 01, 2025 00:00:00").getTime();
+
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    
+    
+    var distance = countDownDate - now;
+    
+    
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    
+    document.getElementById("cronometru").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    
+    
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "WEDDING TIME";
+    }
+}, 1000);
